e6036b760fa1d9d96db21baa13650743769b4e34
M1847\b.botov <b.botov@pixonic.com>
2019-08-26 08:46:56 +0300

    fix GoogleAuth

--- BuyList/BuyList/AppDelegate.swift
+++ BuyList/BuyList/AppDelegate.swift
@@ -7,80 +7,61 @@
 //
 
 import UIKit
-import GoogleSignIn
+//import GoogleSignIn
+import OAuthSwift
 import IQKeyboardManagerSwift
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, open url: URL, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
         IQKeyboardManager.shared.enable = true
         
-        GIDSignIn.sharedInstance().clientID = "488796121076-402aph23b8avbhh2l868oqkeg70ojlid.apps.googleusercontent.com"
-        GIDSignIn.sharedInstance().delegate = self
-//        GIDSignIn.sharedInstance().scopes.append("https://www.buy-list.cloud/auth/login/google-oauth2/")
-        
+//        GIDSignIn.sharedInstance().clientID = ""
+//
+//        GIDSignIn.sharedInstance().delegate = self
+////        GIDSignIn.sharedInstance().scopes.append("https://www.buy-list.cloud/auth/login/google-oauth2/")
         return true
     }
+
     
     @available(iOS 9.0, *)
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool {
-        return GIDSignIn.sharedInstance().handle(url)
+        OAuthSwift.handle(url: url)
+//        return GIDSignIn.sharedInstance().handle(url)
+        return true
     }
     
-    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!,
-              withError error: Error!) {
-        if let error = error {
-            if (error as NSError).code == GIDSignInErrorCode.hasNoAuthInKeychain.rawValue {
-                print("The user has not signed in before or they have since signed out.")
-            } else {
-                print("\(error.localizedDescription)")
-            }
-            return
-        }
-//        // Perform any operations on signed in user here.
-//        let userId = user.userID                  // For client-side use only!
-//        let idToken = user.authentication.idToken // Safe to send to the server
-//        let fullName = user.profile.name
-//        let givenName = user.profile.givenName
-//        let familyName = user.profile.familyName
-//        let email = user.profile.email
-//        // ...
-        
-//        Переход после успешной авторизации.
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let initialViewController = storyboard.instantiateViewController(withIdentifier: "ListController") as! ListController
-        self.window?.rootViewController = initialViewController
-        self.window?.makeKeyAndVisible()
-    }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
+//    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!,
+//              withError error: Error!) {
+//        if let error = error {
+//            if (error as NSError).code == GIDSignInErrorCode.hasNoAuthInKeychain.rawValue {
+//                print("The user has not signed in before or they have since signed out.")
+//            } else {
+//                print("\(error.localizedDescription)")
+//            }
+//            return
+//        }
+////        // Perform any operations on signed in user here.
+////        let userId = user.userID                  // For client-side use only!
+////        let idToken = user.authentication.idToken // Safe to send to the server
+////        let fullName = user.profile.name
+////        let givenName = user.profile.givenName
+////        let familyName = user.profile.familyName
+////        let email = user.profile.email
+////        // ...
+//
+////        Переход после успешной авторизации.
+//        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+//        let initialViewController = storyboard.instantiateViewController(withIdentifier: "ListController") as! ListController
+//        self.window?.rootViewController = initialViewController
+//        self.window?.makeKeyAndVisible()
+//    }
 
 }
 
--- BuyList/BuyList/LoginFromController.swift
+++ BuyList/BuyList/LoginFromController.swift
@@ -8,16 +8,42 @@
 
 import Foundation
 import UIKit
-import GoogleSignIn
+//import GoogleSignIn
+import OAuthSwift
 
 class LoginFromController: UIViewController {
     
-    @IBOutlet weak var signInButton: GIDSignInButton!
+    private func presentAlert(_ title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    
+//    @IBOutlet weak var signInButton: GIDSignInButton!
+    
+    @IBAction private func share(_ sender: AnyObject) {
+        let oauthswift = OAuth2Swift(
+            consumerKey: "",         // [1] Enter google app settings
+            consumerSecret: "",        // No secret required
+            authorizeUrl: "http://www.buy-list.cloud/auth/login/google-oauth2/",
+            accessTokenUrl: "",
+            responseType: "code"
+        )
+        
+        oauthswift.allowMissingStateCheck = true
+        oauthswift.authorizeURLHandler = SafariURLHandler(viewController: self, oauthSwift: oauthswift)
+        
+        guard let rwURL = URL(string: "http://www.google.com") else { return }
+        oauthswift.authorize(withCallbackURL: rwURL, scope: "", state: "", success: { (credential, response, parameters) in
+        }) { (error) in
+            self.presentAlert("Error", message: error.localizedDescription)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        GIDSignIn.sharedInstance()?.presentingViewController = self
+//        GIDSignIn.sharedInstance()?.presentingViewController = self
 
         
         // Automatically sign in the user.
